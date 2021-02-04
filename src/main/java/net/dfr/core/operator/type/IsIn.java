d7922bd2148b5020b601ed800e6db3c7f6040021
Marcelo Portilho <marceloport@gmail.com>
2021-02-04 14:33:04 -0300

    Operations on collection, bug fixes

--- src/main/java/net/dfr/core/operator/type/IsIn.java
+++ src/main/java/net/dfr/core/operator/type/IsIn.java
@@ -2,6 +2,6 @@ package net.dfr.core.operator.type;
 
 import net.dfr.core.operator.FilterOperator;
 
-public interface NotIn<T> extends FilterOperator<T> {
+public interface IsIn<T> extends FilterOperator<T> {
 
 }
--- src/main/java/net/dfr/core/operator/type/IsNotIn.java
+++ src/main/java/net/dfr/core/operator/type/IsNotIn.java
@@ -2,6 +2,6 @@ package net.dfr.core.operator.type;
 
 import net.dfr.core.operator.FilterOperator;
 
-public interface In<T> extends FilterOperator<T> {
+public interface IsNotIn<T> extends FilterOperator<T> {
 
 }
