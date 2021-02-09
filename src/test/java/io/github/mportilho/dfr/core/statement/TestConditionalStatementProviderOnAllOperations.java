0eba7ecfa05b47212356d283e402a5bb47d125f7
Marcelo Portilho <marceloport@gmail.com>
2021-02-09 17:38:13 -0300

    Adding license to test source files

--- src/test/java/io/github/mportilho/dfr/core/statement/TestConditionalStatementProviderOnAllOperations.java
+++ src/test/java/io/github/mportilho/dfr/core/statement/TestConditionalStatementProviderOnAllOperations.java
@@ -1,3 +1,25 @@
+/*MIT License
+
+Copyright (c) 2021 Marcelo Portilho
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
 package io.github.mportilho.dfr.core.statement;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,9 +33,6 @@ import io.github.mportilho.dfr.core.operator.type.IsIn;
 import io.github.mportilho.dfr.core.operator.type.IsNotIn;
 import io.github.mportilho.dfr.core.operator.type.IsNotNull;
 import io.github.mportilho.dfr.core.operator.type.IsNull;
-import io.github.mportilho.dfr.core.statement.ConditionalStatement;
-import io.github.mportilho.dfr.core.statement.ConditionalStatementProvider;
-import io.github.mportilho.dfr.core.statement.LogicType;
 import io.github.mportilho.dfr.core.statement.interfaces.queries.ComparisonOperations;
 import io.github.mportilho.dfr.core.statement.interfaces.queries.OtherComparisonOperations;
 import io.github.mportilho.dfr.core.statement.interfaces.queries.StringComparisonOperations;
